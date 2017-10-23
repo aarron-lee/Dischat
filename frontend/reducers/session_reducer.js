aae5c3cb48c7a5a1b1b8897b484dbd803103a15f
Aarron Lee <aclee91@gmail.com>
2017-10-23 19:18:27 -0400

    create: session_reducer with default state, handles for RECEIVE_CURRENT_USER action

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,22 @@
+
+import {RECEIVE_CURRENT_USER,
+        RECEIVE_SESSION_ERRORS}
+        from '../actions/session_actions';
+
+
+function sessionReducer( state = {}, action){
+  Object.freeze(state);
+  let newState;
+  switch(action.type){
+    case RECEIVE_CURRENT_USER:
+      // action.user
+      newState = { currentUser: action.user }
+      return newState;
+    default:
+      return state;
+  }// end switch
+}// end sessionReducer
+
+
+
+export default sessionReducer;

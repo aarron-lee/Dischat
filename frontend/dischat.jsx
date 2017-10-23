6852eb1d0bffa9881e325e6cb26de6d10fe65e93
Aarron Lee <aclee91@gmail.com>
2017-10-23 18:43:56 -0400

    Add: React entry file created, modified root.html for to display an error message, added webpack to package.json

---
+++ frontend/dischat.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  const root = document.getElementById('mount-point');
+
+  ReactDOM.render(<h2>React Injection working</h2>, root);
+
+});

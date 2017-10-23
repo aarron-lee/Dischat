b87b3884794cec4d621d188de7d783ec9c1fe065
Aarron Lee <aclee91@gmail.com>
2017-10-23 17:28:22 -0400

    create: SessionsController w/ api/create and api/destroy calls

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create]
+    resource :sessions, only: [:create, :destroy]
   end
 
 end

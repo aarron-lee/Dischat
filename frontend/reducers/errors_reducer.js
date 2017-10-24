a18f18bace771d4261b61dc744b42a36d9a9d129
Aarron Lee <aclee91@gmail.com>
2017-10-24 12:01:34 -0400

    create: errorsReducer to handle for invalid session actions

---
+++ frontend/reducers/errors_reducer.js
@@ -0,0 +1,22 @@
+
+import {RECEIVE_CURRENT_USER,
+        RECEIVE_SESSION_ERRORS}
+        from '../actions/session_actions';
+
+
+function errorsReducer( state = [], action){
+  Object.freeze(state);
+  let newState;
+  switch(action.type){
+    case RECEIVE_SESSION_ERRORS:
+      return newState = action.errors.responseJSON;
+    case RECEIVE_CURRENT_USER:
+      return [];
+    default:
+      return state;
+  }// end switch
+}// end errorsReducer
+
+
+
+export default errorsReducer;

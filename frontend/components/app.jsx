95d2694314e583ea3d0163cf5fc35391302ea5c4
Aarron Lee <aclee91@gmail.com>
2017-10-24 11:45:13 -0400

    create: greeting component w/ logout button when loggedIn

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import SessionFormContainer from './session_form/session_form_container';
 import {Route} from 'react-router-dom';
+import GreetingContainer from './greeting/greeting_container';
 
 const App = () =>{
 
   return (
     <div>
+      <GreetingContainer />
       <Route path="/login" component={SessionFormContainer} />
       <Route path="/signup" component={SessionFormContainer} />
     </div>
---
+++ frontend/components/greeting/greeting_container.jsx
@@ -0,0 +1,56 @@
+
+import React from 'react';
+import { connect } from 'react-redux';
+import { logout } from '../../actions/session_actions';
+import { Link } from 'react-router-dom';
+
+
+class Greeting extends React.Component{
+
+  render(){
+    if( this.props.currentUser ){
+      return (
+        <div>
+          <h2> Hi, {this.props.currentUser.username }</h2>
+          <button onClick={() => this.props.logout()}>Logout</button>
+        </div>
+      )
+    }
+
+    return(
+      <div>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign Up</Link>
+      </div>
+    )
+
+
+  }
+
+}
+
+
+
+function mapStateToProps(state, ownProps){
+  if( state.session.currentUserId ){
+    // logged in, pass on currentUser
+    return {
+      currentUser: state.entities.users[state.session.currentUserId],
+    };
+  }
+  return {
+    currentUser: undefined
+  };
+}
+
+function mapDispatchToProps(dispatch, ownProps){
+  return {
+    logout: () => dispatch( logout() ),
+  };
+}
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Greeting);

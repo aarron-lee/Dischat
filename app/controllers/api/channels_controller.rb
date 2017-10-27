116d8f77d7ca7f3528650a2ef5364b78112f54b2
Aarron Lee <aclee91@gmail.com>
2017-10-27 11:45:35 -0400

    create: channel api calls, and json view

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -1,7 +1,17 @@
 class Api::ChannelsController < ApplicationController
 
+  before_action :ensure_logged_in, only: [:create, :update]
+
+
   #create channel
   def create
+    @channel = Channel.new(channel_params)
+
+    if(@channel.save)
+      render :show, status: 200
+    else
+      render json: @channel.errors.full_messages, status: 400
+    end
   end
 
   #TODO get messages for channel
@@ -9,6 +19,24 @@ class Api::ChannelsController < ApplicationController
 
   # update channel name or description
   def update
+    @channel = Channel.find(params[:id])
+
+    if(@channel)
+      @channel.update_attributes(channel_params)
+      if(@channel.save)
+        render :show, status: 200
+      else
+        render json: @channel.errors.full_messages, status: 401
+      end
+    else
+      render json: "Channel doesn't exist", status: 400
+    end
+  end
+
+  private
+
+  def channel_params
+    params.require(:channel).permit(:name, :chatroom_id, :description)
   end
 
 end

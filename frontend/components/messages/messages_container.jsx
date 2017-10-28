5edf4e6029293df35fcec208898a473f57e60aca
Aarron Lee <aclee91@gmail.com>
2017-10-28 13:39:34 -0400

    create: basic messages container

---
+++ frontend/components/messages/messages_container.jsx
@@ -0,0 +1,70 @@
+
+import React from 'react';
+import { connect } from 'react-redux';
+import { logout } from '../../actions/session_actions';
+import { openModal, closeModal } from '../../actions/modal_actions';
+import { Link, Redirect, withRouter } from 'react-router-dom';
+
+
+class MessagesList extends React.Component{
+
+  constructor(props){
+    super(props);
+
+  }
+
+
+  formModal(){
+    // <MyModal component={myForm}  closeModal={this.props.closeModal}/>
+    return(
+        <div className="modal-backdrop" onClick={() => this.props.closeModal()} >
+          <AddChannelForm
+            chatroom={this.props.chatroom}
+            addChannel={this.props.addChannel}
+            errors={this.props.errors} />
+        </div>);
+
+  }
+
+
+
+
+  render(){
+
+    return (
+      <div>
+        Messages!
+      </div>
+    );
+  }// end render
+
+  componentWillReceiveProps(newProps){
+
+  }// end componentWillReceiveProps
+
+  componentDidMount(){
+  }
+
+}
+
+
+
+function mapStateToProps(state, ownProps){
+
+  return {
+    modal: state.ui.modal,
+    errors: state.errors,};
+}
+
+function mapDispatchToProps(dispatch, ownProps){
+  return {
+    openModal: (modal) => dispatch( openModal(modal) ),
+    closeModal: () => dispatch( closeModal() ),
+  };
+}
+
+
+export default withRouter(connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MessagesList));

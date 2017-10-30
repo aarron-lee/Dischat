0632e6c6fddd35df6ca5bd5706dce21d5df1c99c
Aarron Lee <aclee91@gmail.com>
2017-10-30 08:14:20 -0400

    add: thunks for messages

--- frontend/actions/message_actions.js
+++ frontend/actions/message_actions.js
@@ -20,12 +20,81 @@ export const receiveMessage = (message)=>{
   }
 }
 
-export const deleteMessage = (messageId) =>{
+export const deleteMessage = (message) =>{
   return {
     type: DELETE_MESSAGE,
-    messageId
+    messageId: message.id
   }
 }
 
 
 // thunks -----------------
+
+// getMessages
+// createMessage
+// updateMessage
+// deleteMessage
+
+export const getMessages = (channelId) =>{
+  return (dispatch)=>{
+    let success = (messages) => dispatch(receiveMessages(messages));
+    let failure = (errors) => dispatch({type: "RECEIVE_ERRORS", errors});
+    return MessageAPIUtil.getMessages(channelId).next(
+      success,
+      failure
+    );
+  }
+}
+
+export const createMessage = (message) =>{
+  return (dispatch)=>{
+    let success = (message) => dispatch(receiveMessage(message));
+    let failure = (errors) => dispatch({type: "RECEIVE_ERRORS", errors});
+    return MessageAPIUtil.createMessage(message).next(
+      success,
+      failure
+    );
+  }
+}
+
+export const updateMessage = (message) =>{
+  return (dispatch)=>{
+    let success = (message) => dispatch(receiveMessage(message));
+    let failure = (errors) => dispatch({type: "RECEIVE_ERRORS", errors});
+    return MessageAPIUtil.updateMessage(message).next(
+      success,
+      failure
+    );
+  }
+}
+
+export const deleteMessage = (messageId) =>{
+  return (dispatch)=>{
+    let success = (message) => dispatch(deleteMessage(message));
+    let failure = (errors) => dispatch({type: "RECEIVE_ERRORS", errors});
+    return MessageAPIUtil.deleteMessage(messageId).next(
+      success,
+      failure
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//

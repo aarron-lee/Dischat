c0711a68636422cc50df1a9a620374aae98a9a25
Aarron Lee <aclee91@gmail.com>
2017-10-31 08:18:11 -0400

    create: message actions, thunks, ajax calls

---
+++ frontend/actions/message_actions.js
@@ -0,0 +1,75 @@
+
+import * as MessageAPIUtil from '../util/message_api_util';
+
+
+export const RECEIVE_MESSAGES = "RECEIVE_MESSAGES";
+export const RECEIVE_MESSAGE = "RECEIVE_MESSAGE";
+export const DELETE_MESSAGE = "DELETE_MESSAGE";
+
+export const receiveMessages = (messages)=>{
+  return {
+    type: RECEIVE_MESSAGES,
+    messages
+  }
+}
+
+export const receiveMessage = (message)=>{
+  return {
+    type: RECEIVE_MESSAGE,
+    message
+  }
+}
+
+export const receiveDeleteMessage = (message) =>{
+  return {
+    type: DELETE_MESSAGE,
+    messageId: message.id
+  }
+}
+
+
+// thunks -----------------
+
+export const getMessages = (channelId) =>{
+  return (dispatch)=>{
+    let success = (payload) => dispatch(receiveMessages(payload.messages));
+    let failure = (errors) => dispatch({type: "RECEIVE_ERRORS", errors});
+    return MessageAPIUtil.getMessages(channelId).then(
+      success,
+      failure
+    );
+  }
+}
+
+export const createMessage = (message) =>{
+  return (dispatch)=>{
+    let success = (message) => dispatch(receiveMessage(message));
+    let failure = (errors) => dispatch({type: "RECEIVE_ERRORS", errors});
+    return MessageAPIUtil.createMessage(message).then(
+      success,
+      failure
+    );
+  }
+}
+
+export const updateMessage = (message) =>{
+  return (dispatch)=>{
+    let success = (message) => dispatch(receiveMessage(message));
+    let failure = (errors) => dispatch({type: "RECEIVE_ERRORS", errors});
+    return MessageAPIUtil.updateMessage(message).then(
+      success,
+      failure
+    );
+  }
+}
+
+export const deleteMessage = (messageId) =>{
+  return (dispatch)=>{
+    let success = (message) => dispatch(receiveDeleteMessage(message));
+    let failure = (errors) => dispatch({type: "RECEIVE_ERRORS", errors});
+    return MessageAPIUtil.deleteMessage(messageId).then(
+      success,
+      failure
+    );
+  }
+}

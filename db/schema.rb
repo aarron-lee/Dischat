44b5d1ce0e0b87d037a88aec43b5d0e113b04cd4
Aarron Lee <aclee91@gmail.com>
2017-11-02 11:48:40 -0400

    refactored channel model to allow nil for chatroom relation

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101225015) do
+ActiveRecord::Schema.define(version: 20171102144658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "channels", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "chatroom_id", null: false
+    t.integer "chatroom_id"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

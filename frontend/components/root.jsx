c811ab4830ee4854a78de9e6210783662cf6bba1
Aarron Lee <aclee91@gmail.com>
2017-11-09 10:51:48 -0500

    bugfix: setup httpsredirect

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,14 +3,18 @@ import {Provider} from 'react-redux';
 import App from './app';
 import React from 'react';
 import {HashRouter} from 'react-router-dom';
+import HttpsRedirect from 'react-https-redirect';
+
 
 const Root = ({store}) =>{
 
   return (
     <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <HttpsRedirect>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </HttpsRedirect>
     </Provider>
   );
 
